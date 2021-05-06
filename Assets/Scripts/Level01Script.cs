af57adb15e4df0c20aa63e03efbf74b34c61d89a
Cullen Pierce <cap180007@utdallas.edu>
2021-05-05 23:25:57 -0500

    MainMenu
    Added main menu

--- Assets/Scripts/Level01Script.cs
+++ Assets/Scripts/Level01Script.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Level01Script : MonoBehaviour
 {
@@ -16,7 +17,7 @@ public class Level01Script : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Application.Quit();
+            SceneManager.LoadScene("MainMenu");
         }
     }
 }
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
